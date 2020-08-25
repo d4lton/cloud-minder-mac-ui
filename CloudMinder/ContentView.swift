ece68c62fddfdc78330d77a3a17313423cdaae14
Dana Basken <dana@nanigans.com>
2020-08-25 14:45:05 -0400

    Initial Commit

---
+++ CloudMinder/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CloudMinder
+//
+//  Created by Dana Basken on 8/25/20.
+//  Copyright © 2020 Dana Basken. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
