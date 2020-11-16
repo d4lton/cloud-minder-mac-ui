8b72094fb9780d02e428cb25a25af16d59b2f43a
Dana Basken <dbasken@gmail.com>
2020-11-16 11:06:33 -0500

    added path check for gcloud binary, updated some text

--- CloudMinder/AdvancedPreferenceViewController.swift
+++ CloudMinder/AdvancedPreferenceViewController.swift
@@ -19,14 +19,44 @@ class AdvancedPreferenceViewController: NSViewController, PreferencePane {
     override var nibName: NSNib.Name? { "AdvancedPreferenceViewController" }
 
     @IBOutlet weak var gcloudPathTextField: NSTextField!
+    @IBOutlet weak var gcloudValidPathImageCell: NSImageCell!
+    
+    var timer: Timer!
+    let fileManager = FileManager.default
 
     override func viewDidLoad() {
         super.viewDidLoad()
         loadSettings()
     }
+    
+    func showValidPathIcon(name: String) {
+        DispatchQueue.main.async {
+            self.gcloudValidPathImageCell.image = NSImage(named: name)
+        }
+    }
+    
+    override func viewWillAppear() {
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { timer in
+            var isDir : ObjCBool = false
+            if (FileManager.default.fileExists(atPath: self.gcloudPathTextField.stringValue, isDirectory: &isDir)) {
+                if (isDir.boolValue) {
+                    self.showValidPathIcon(name: "IconError")
+                } else {
+                    if (FileManager.default.isExecutableFile(atPath: self.gcloudPathTextField.stringValue)) {
+                        self.showValidPathIcon(name: "IconGreen")
+                    } else {
+                        self.showValidPathIcon(name: "IconError")
+                    }
+                }
+            } else {
+                self.showValidPathIcon(name: "IconError")
+            }
+        }
+    }
 
     override func viewWillDisappear() {
         gcloudPathAction(self)
+        timer.invalidate()
     }
 
     @IBAction func gcloudPathAction(_ sender: Any) {
